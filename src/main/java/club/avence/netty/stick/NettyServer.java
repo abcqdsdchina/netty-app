3d84fdc981c52c1af26528b73c1bcdf20d65e9d5
钱程 <abcqdsdchina@126.com>
2020-05-27 19:34:02 +0800

    代码优化

--- src/main/java/club/avence/netty/stick/NettyServer.java
+++ src/main/java/club/avence/netty/stick/NettyServer.java
@@ -10,23 +10,26 @@ import lombok.Cleanup;
 
 import java.net.InetSocketAddress;
 
+/**
+ * @author qian3
+ */
 public class NettyServer {
 
-    private final int port = 6001;
+    private static final int PORT = 6001;
 
     public void start() throws InterruptedException {
         final NettyServerHandler handler = new NettyServerHandler();
         @Cleanup ClosableNioEventLoopGroup group = new ClosableNioEventLoopGroup();
-        ServerBootstrap boostrap = new ServerBootstrap().group(group)
+        ServerBootstrap bootstrap = new ServerBootstrap().group(group)
                 .channel(NioServerSocketChannel.class)
-                .localAddress(new InetSocketAddress(port))
+                .localAddress(new InetSocketAddress(PORT))
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(SocketChannel channel) {
                         channel.pipeline().addLast(handler);
                     }
                 });
-        ChannelFuture f = boostrap.bind().sync();
+        ChannelFuture f = bootstrap.bind().sync();
         f.channel().closeFuture().sync();
     }
 
