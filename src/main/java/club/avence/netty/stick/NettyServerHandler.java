c83ec0709d6f3c20d8bf0a48818c1918d27e33ed
钱程 <abcqdsdchina@126.com>
2020-05-27 20:04:35 +0800

    代码优化

--- src/main/java/club/avence/netty/stick/NettyServerHandler.java
+++ src/main/java/club/avence/netty/stick/NettyServerHandler.java
@@ -11,20 +11,23 @@ import lombok.extern.slf4j.Slf4j;
 import java.text.MessageFormat;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * @author qian3
+ */
 @Slf4j
 @ChannelHandler.Sharable
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
 
-    private static final String MESSAGE_TEMPALTE = "Hello, {0}! Your message has been recieved.";
+    private static final String MESSAGE_TEMPLATE = "Hello, {0}! Your message has been recieved.";
 
-    private static final AtomicInteger count = new AtomicInteger(0);
+    private static final AtomicInteger COUNT = new AtomicInteger(0);
 
     @Override
     public void channelRead(ChannelHandlerContext context, Object buffer) {
         String message = ((ByteBuf) buffer).toString(CharsetUtil.UTF_8);
         log.info("接收到消息：{}", message);
-        log.info("接收到的数量：{}", count.incrementAndGet());
-        String response = MessageFormat.format(MESSAGE_TEMPALTE, message);
+        log.info("接收到的数量：{}", COUNT.incrementAndGet());
+        String response = MessageFormat.format(MESSAGE_TEMPLATE, message);
         context.write(Unpooled.copiedBuffer(response, CharsetUtil.UTF_8));
         log.info("发送了响应消息：{}", response);
     }
